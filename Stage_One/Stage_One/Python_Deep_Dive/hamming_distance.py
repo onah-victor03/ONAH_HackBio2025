f98d54edc66895928ab0032cb0461d04cbee4097
onah-victor03 <onahvictor606@gmail.com>
2025-11-13 14:56:07 +0100

    Add files via upload

---
+++ Stage_One/Stage_One/Python_Deep_Dive/hamming_distance.py
@@ -0,0 +1,23 @@
+"""
+HackBio Internship - Stage 1 Python Task
+Team: Glycine
+
+# Task: Write a Python function for calculating the hamming distance between your slack username ('Onah Victor') and twitter/X handle (@onahvictor606@)
+# Author: Onah Victor
+"""
+
+def hamming_distance(str1, str2):
+    # pad shorter string with spaces
+    max_len = max(len(str1), len(str2))
+    str1 = str1.ljust(max_len)
+    str2 = str2.ljust(max_len)
+
+    distance = sum(1 for a, b in zip(str1, str2) if a != b)
+    return distance
+
+if __name__ == "__main__":
+    slack = "Onah Victor"
+    twitter = "@onahvictor606@"
+    print("Slack:", slack)
+    print("Twitter:", twitter)
+    print("Hamming distance:", hamming_distance(slack, twitter))
